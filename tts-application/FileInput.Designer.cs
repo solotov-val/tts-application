da94d8bee202240a07424ee59347a2daa0a3ce86
Alex Pastore <pasale007@icloud.com>
2022-05-12 10:04:17 +0200

    pasale/textinput with functions to check if the textlength is okay

--- tts-application/FileInput.Designer.cs
+++ tts-application/FileInput.Designer.cs
@@ -28,20 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(109, 242);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(394, 326);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(109, 608);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open file";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(391, 608);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 31);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Convert";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(759, 615);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 45);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Download";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(771, 218);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 31);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(771, 268);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 31);
+            this.numericUpDown2.TabIndex = 5;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(741, 576);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(180, 33);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(653, 218);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Speed";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(658, 268);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Volume";
             // 
             // FileInput
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(974, 764);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
             this.Name = "FileInput";
             this.Text = "FileInput";
             this.Load += new System.EventHandler(this.FileInput_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- tts-application/TextInput.cs
+++ tts-application/TextInput.cs
@@ -13,11 +13,15 @@ namespace tts_application
     public partial class TextInput : Form
     {
         Form loginPage;
-       
+        String input;
+        
+
         public TextInput(Form f)
         {
             InitializeComponent();
             this.loginPage = f;
+            limitExcceded.Hide();
+            charLimitExcceded.Hide();
         }
 
         public void setStandLang(String language)
@@ -35,11 +39,6 @@ namespace tts_application
 
         }
 
-
-        public void convertClick()
-        {
-
-        }
 
         public void basicsClick()
         {
@@ -61,6 +60,67 @@ namespace tts_application
         private void TextInput_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void buttonConvert_Click(object sender, EventArgs e)
+        {
+            input= userInput.Text;
+        }
+
+        private void userInput_TextChanged(object sender, EventArgs e)
+        {
+            wordCounter(userInput.Text);
+            charCounter(userInput.Text);
+        }
+
+        private void wordCounter(String text)
+        {
+
+            int counter = 0;
+            for (int i = 0; i < text.Length - 1; i++)
+            {
+                if(text[i] == ' ' && Char.IsLetter(text[i + 1]) && (i > 0)) {
+                    counter++;
+
+                }    
+            }
+            counter++;
+
+            if(counter > 50)
+            {
+                limitExcceded.Show();
+                buttonConvert.Enabled = false;
+            }
+            else
+            {
+                limitExcceded.Hide();
+                buttonConvert.Enabled = true;
+            }
+            wordCount.Text = "" + counter;
+        }
+
+        private void charCounter(String text)
+        {
+
+            int counter = 0;
+
+            foreach(char c in text)
+            {
+                counter++;
+            }
+
+
+            if (counter > 250)
+            {
+                charLimitExcceded.Show();
+                buttonConvert.Enabled = false;
+            }
+            else
+            {
+                charLimitExcceded.Hide();
+                buttonConvert.Enabled = true;
+            }
+            charCount.Text = "" + counter;
         }
     }
 }
