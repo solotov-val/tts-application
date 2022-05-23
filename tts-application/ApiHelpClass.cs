1f6caaaffdcdd342661aa7146dbe3e502ae54bf2
solotov2k <prillerpatrick@gmail.com>
2022-05-23 10:47:08 +0200

    pripat/changed translate parameters

--- tts-application/ApiHelpClass.cs
+++ tts-application/ApiHelpClass.cs
@@ -12,23 +12,48 @@ namespace tts_application
     {
         public static String tts(String language, String speaker, String text)
         {
-            bool writeSuccessful=false;
-            var inputFileName = "tempfile_"+DateTime.Now.ToString().Replace(":", "-").Replace(" ", "_")+".txt";
+            bool writeSuccessful = false;
+            var outputFileName = "tempfile.txt";
 
             try
             {
-                File.WriteAllText(inputFileName, text, Encoding.UTF8);
-                writeSuccessful=true;
+                File.WriteAllText(outputFileName, text, Encoding.UTF8);
+                writeSuccessful = true;
             }
             catch (Exception)
             {
                 MessageBox.Show("Temporary file could not be saved!");
             }
 
-            if(writeSuccessful==true)
+            if (writeSuccessful == true)
             {
-                String[] args = {inputFileName, language, speaker};
+                String[] args = { outputFileName, language, speaker };
                 _ = ConvertTTS.Main(args);
+            }
+
+            return "ERROR API-HELP-CLASS";
+        }
+
+        public static String translate(String authKey, String inputLang, String outputLang, String text)
+        {
+            bool writeSuccessful = false;
+            var outputFileName = "tempfile2_" + DateTime.Now.ToString().Replace(":", "-").Replace(" ", "_") + ".txt";
+            //authKey = "48b840d9-957fe91b-ff3d-d5616d26a7b3:fx";
+
+            try
+            {
+                File.WriteAllText(outputFileName, text, Encoding.UTF8);
+                writeSuccessful = true;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Temporary file could not be saved!");
+            }
+
+            if (writeSuccessful == true)
+            {
+                String[] args = { authKey, outputFileName, inputLang, outputLang };
+                _ = Translate.Main(args);
             }
 
             return "ERROR API-HELP-CLASS";
