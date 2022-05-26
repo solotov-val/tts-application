7815da0939ae1cc7643e6810278936977c25391e
Michael Huber <Michael.Huber_3006@snets.it>
2022-05-26 08:29:36 +0200

    hubmic/UserList now works in MainLoginWindow

--- XamlUI/MainLoginWindow.xaml.cs
+++ XamlUI/MainLoginWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using tts_application;
 using XamlUI.MVVM.View;
 
 namespace XamlUI
